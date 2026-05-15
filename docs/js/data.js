735fa70dbcd4a8baf0e845e87587c20d4401bebe
Kévin <54324251+kevinraphael95@users.noreply.github.com>
2026-05-15 22:31:24 +0200

    Update data.js

--- docs/js/data.js
+++ docs/js/data.js
@@ -33,6 +33,15 @@ CLASSES DE MONSTRES (Mogapedia) :
 */
 
 /*
+TAILLE
+- Petit : <10m
+- Moyen : 10m - 18m
+- Grand : 18m - 30m
+- Géant : 30m - 100m
+- Colossal : >100m
+*/
+
+/*
 STATUTS ET FLÉAUX (Ailments & Blights) :
 - Boue / Neige / Toile (Muddy / Snowy / Webbed)
 - Étourdissement (Stun)
