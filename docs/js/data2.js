df8b9a4405133cd40055922222bd00f651ef7b55
Kévin <54324251+kevinraphael95@users.noreply.github.com>
2026-05-15 20:03:02 +0200

    Create data2.js

---
+++ docs/js/data2.js
@@ -0,0 +1,185 @@
+// --- GÉNÉRATION FRONTIER ---
+  {n:"Abiorugu",c:"Wyverne brute",g:"Frontier",e:"🔥 Feu",s:2,h:"Forêt",d:4}, // ~16.2 m
+  {n:"Akura Jebia",c:"Carapaceon",g:"Frontier",e:"Aucun",s:2,h:"Marais",d:4}, // ~13.8 m
+  {n:"Akura Vashimu",c:"Carapaceon",g:"Frontier",e:"Aucun",s:2,h:"Désert",d:4}, // ~13.8 m
+  {n:"Akura Vashimu Zénith",c:"Carapaceon",g:"Frontier",e:"Aucun",s:3,h:"Désert",d:5}, // ~15.5 m
+  {n:"Anorupatisu",c:"Wyverne volante",g:"Frontier",e:"🧊 Glace / 💧 Eau",s:3,h:"Tundra",d:5}, // ~21.3 m
+  {n:"Anorupatisu Zénith",c:"Wyverne volante",g:"Frontier",e:"🧊 Glace",s:3,h:"Tundra",d:5}, // ~23.0 m
+  {n:"Aruganosu",c:"Wyverne aquatique",g:"Frontier",e:"🧊 Glace",s:2,h:"Volcan",d:4}, // ~15.4 m
+  {n:"Baruragaru",c:"Wyverne léviathan",g:"Frontier",e:"💧 Eau",s:3,h:"Marais",d:4}, // ~22.8 m
+  {n:"Baruragaru Zénith",c:"Wyverne léviathan",g:"Frontier",e:"💧 Eau",s:3,h:"Marais",d:5}, // ~24.5 m
+  {n:"Berukyurosu",c:"Wyverne volante",g:"Frontier",e:"⚡ Foudre",s:3,h:"Montagne",d:4}, // ~19.1 m
+  {n:"Blangonga Zénith",c:"Bête à crocs",g:"Frontier",e:"🧊 Glace",s:2,h:"Tundra",d:5}, // ~14.2 m
+  {n:"Bogabadorumu",c:"Wyverne brute",g:"Frontier",e:"Blast",s:3,h:"Volcan",d:5}, // ~23.4 m
+  {n:"Bogabadorumu bombardier",c:"Wyverne brute",g:"Frontier",e:"Blast",s:3,h:"Volcan",d:5}, // ~23.4 m
+  {n:"Deviljho des quatre rois célestes",c:"Wyverne brute",g:"Frontier",e:"🐉 Dragon",s:3,h:"Ruines",d:5}, // ~25.0 m
+  {n:"Dhisufiroa",c:"Dragon ancien",g:"Frontier",e:"🔥 Feu / 🧊 Glace",s:3,h:"Ruines",d:5}, // ~32.4 m
+  {n:"Diorex",c:"Wyverne volante",g:"Frontier",e:"⚡ Foudre",s:3,h:"Forêt",d:5}, // ~20.7 m
+  {n:"Doragyurosu",c:"Wyverne volante",g:"Frontier",e:"🐉 Dragon",s:3,h:"Montagne",d:4}, // ~19.1 m
+  {n:"Doragyurosu Zénith",c:"Wyverne volante",g:"Frontier",e:"🐉 Dragon",s:3,h:"Montagne",d:5}, // ~21.0 m
+  {n:"Duremudira",c:"Dragon ancien / ???",g:"Frontier",e:"🧊 Glace",s:3,h:"Ruines",d:5}, // ~28.0 m
+  {n:"Duremudira arrogant",c:"Dragon ancien / ???",g:"Frontier",e:"🧊 Glace / Poison",s:3,h:"Ruines",d:5}, // ~28.0 m
+  {n:"Dyuragaua",c:"Wyverne volante",g:"Frontier",e:"🧊 Glace",s:2,h:"Ruines",d:4}, // ~15.9 m
+  {n:"Elzelion",c:"Dragon ancien",g:"Frontier",e:"🔥 Feu / 🧊 Glace",s:3,h:"Ruines",d:5}, // ~24.1 m
+  {n:"Elzelion ardent glacial",c:"Dragon ancien",g:"Frontier",e:"🔥 Feu / 🧊 Glace",s:3,h:"Ruines",d:5}, // ~24.1 m
+  {n:"Espinas",c:"Wyverne volante",g:"Frontier",e:"🔥 Feu / Poison",s:3,h:"Forêt",d:4}, // ~19.2 m
+  {n:"Espinas cendreuse",c:"Wyverne volante",g:"Frontier",e:"🔥 Feu / Poison",s:3,h:"Désert",d:4}, // ~19.2 m
+  {n:"Espinas de feu",c:"Wyverne volante",g:"Frontier",e:"🔥 Feu / Poison",s:3,h:"Volcan",d:4}, // ~19.2 m
+  {n:"Espinas Zénith",c:"Wyverne volante",g:"Frontier",e:"Poison",s:3,h:"Forêt",d:5}, // ~21.5 m
+  {n:"Farunokku",c:"Wyverne rapace",g:"Frontier",e:"⚡ Foudre",s:1,h:"Forêt",d:3}, // ~9.4 m
+  {n:"Forokururu",c:"Wyverne rapace",g:"Frontier",e:"Aucun",s:2,h:"Forêt",d:4}, // ~13.2 m
+  {n:"Garuba Daora",c:"Dragon ancien",g:"Frontier",e:"Aucun",s:3,h:"Ruines",d:5}, // ~20.4 m
+  {n:"Gasurabazura",c:"Wyverne brute",g:"Frontier",e:"Poison",s:3,h:"Volcan",d:4}, // ~21.0 m
+  {n:"Gasurabazura Zénith",c:"Wyverne brute",g:"Frontier",e:"Poison",s:3,h:"Volcan",d:5}, // ~23.2 m
+  {n:"Giaorugu",c:"Wyverne brute",g:"Frontier",e:"🧊 Glace",s:2,h:"Tundra",d:4}, // ~16.2 m
+  {n:"Giaorugu Zénith",c:"Wyverne brute",g:"Frontier",e:"🧊 Glace",s:3,h:"Tundra",d:5}, // ~18.5 m
+  {n:"Gogomoa",c:"Bête à crocs",g:"Frontier",e:"Aucun",s:2,h:"Forêt",d:3}, // ~11.8 m
+  {n:"Goruganosu",c:"Wyverne aquatique",g:"Frontier",e:"⚡ Foudre",s:2,h:"Volcan",d:4}, // ~15.4 m
+  {n:"Gravios Zénith",c:"Wyverne volante",g:"Frontier",e:"🔥 Feu",s:3,h:"Volcan",d:5}, // ~24.5 m
+  {n:"Guanzorumu",c:"Dragon ancien",g:"Frontier",e:"🔥 Feu / 🐉 Dragon",s:3,h:"Ruines",d:5}, // ~25.6 m
+  {n:"Guanzorumu souverain",c:"Dragon ancien",g:"Frontier",e:"🔥 Feu / 🐉 Dragon",s:3,h:"Ruines",d:5}, // ~25.6 m
+  {n:"Gureadomosu",c:"Wyverne volante",g:"Frontier",e:"💧 Eau",s:3,h:"Désert",d:4}, // ~23.1 m
+  {n:"Gurenzeburu",c:"Wyverne volante",g:"Frontier",e:"💧 Eau",s:3,h:"Montagne",d:4}, // ~21.4 m
+  {n:"Harudomerugu",c:"Dragon ancien",g:"Frontier",e:"Aucun",s:3,h:"Montagne",d:5}, // ~24.8 m
+  {n:"Harudomerugu Zénith",c:"Dragon ancien",g:"Frontier",e:"Aucun",s:3,h:"Montagne",d:5}, // ~26.5 m
+  {n:"Hermitaur Daimyo Zénith",c:"Carapaceon",g:"Frontier",e:"💧 Eau",s:2,h:"Marais",d:5}, // ~13.0 m (hauteur)
+  {n:"Hypnocatrice azure",c:"Wyverne rapace",g:"Frontier",e:"Aucun",s:2,h:"Forêt",d:3}, // ~12.9 m
+  {n:"Hypnocatrice saison des amours",c:"Wyverne rapace",g:"Frontier",e:"Aucun",s:2,h:"Forêt",d:3}, // ~12.9 m
+  {n:"Hypnocatrice Zénith",c:"Wyverne rapace",g:"Frontier",e:"Aucun",s:2,h:"Forêt",d:5}, // ~14.5 m
+  {n:"Hyujikiki",c:"Wyverne volante",g:"Frontier",e:"Aucun",s:2,h:"Désert",d:4}, // ~16.8 m
+  {n:"Hyujikiki Zénith",c:"Wyverne volante",g:"Frontier",e:"Aucun",s:3,h:"Désert",d:5}, // ~18.5 m
+  {n:"Inagami",c:"Dragon ancien",g:"Frontier",e:"Aucun",s:3,h:"Forêt",d:5}, // ~22.3 m
+  {n:"Inagami Zénith",c:"Dragon ancien",g:"Frontier",e:"Aucun",s:3,h:"Forêt",d:5}, // ~24.0 m
+  {n:"Inconnu (Wyverne volante noire)",c:"Wyverne volante",g:"Frontier",e:"🔥 Feu / Poison",s:3,h:"Ruines",d:5}, // ~18.0 m
+  {n:"Kamu Orugaron",c:"Bête à crocs",g:"Frontier",e:"Aucun",s:2,h:"Marais",d:4}, // ~12.6 m
+  {n:"Keoaruboru",c:"Dragon ancien",g:"Frontier",e:"🔥 Feu",s:4,h:"Ruines",d:5}, // ~72.0 m
+  {n:"Khezu Zénith",c:"Wyverne volante",g:"Frontier",e:"⚡ Foudre",s:2,h:"Cave",d:5}, // ~11.5 m
+  {n:"Kuarusepusu",c:"Wyverne volante",g:"Frontier",e:"⚡ Foudre",s:3,h:"Désert",d:4}, // ~22.5 m
+  {n:"Lavasioth rouge",c:"Wyverne aquatique",g:"Frontier",e:"🔥 Feu",s:3,h:"Volcan",d:4}, // ~19.2 m
+  {n:"Lolo Gougarf",c:"Bête à crocs",g:"Frontier",e:"Aucun",s:2,h:"Forêt",d:4}, // ~13.5 m
+  {n:"Meraginasu",c:"Wyverne volante",g:"Frontier",e:"Aucun",s:3,h:"Cave",d:4}, // ~20.5 m
+  {n:"Mi Ru",c:"Wyverne volante",g:"Frontier",e:"Aucun",s:3,h:"Ruines",d:5}, // ~19.8 m
+  {n:"Mi Ru mystérieux",c:"Wyverne volante",g:"Frontier",e:"Aucun",s:3,h:"Ruines",d:5}, // ~19.8 m
+  {n:"Midogaron",c:"Bête à crocs",g:"Frontier",e:"🔥 Feu",s:2,h:"Volcan",d:4}, // ~13.2 m
+  {n:"Midogaron Zénith",c:"Bête à crocs",g:"Frontier",e:"🔥 Feu",s:2,h:"Volcan",d:5}, // ~14.8 m
+  {n:"Nargacuga fugace",c:"Wyverne volante",g:"Frontier",e:"Aucun",s:3,h:"Forêt",d:5}, // ~21.0 m
+  {n:"Nono Orugaron",c:"Bête à crocs",g:"Frontier",e:"Aucun",s:2,h:"Marais",d:4}, // ~12.1 m
+  {n:"Odibatorasu",c:"Dragon ancien",g:"Frontier",e:"Aucun",s:4,h:"Désert",d:5}, // ~46.5 m
+  {n:"Pariapuria",c:"Wyverne volante",g:"Frontier",e:"💧 Eau",s:3,h:"Cave",d:3}, // ~18.2 m
+  {n:"Pariapuria assoiffé",c:"Wyverne volante",g:"Frontier",e:"💧 Eau",s:3,h:"Cave",d:5}, // ~19.5 m
+  {n:"Plesioth Zénith",c:"Wyverne aquatique",g:"Frontier",e:"💧 Eau",s:3,h:"Désert",d:5}, // ~28.0 m
+  {n:"Poborubarumu",c:"Wyverne brute",g:"Frontier",e:"Aucun",s:3,h:"Montagne",d:4}, // ~26.8 m
+  {n:"Pokaradon",c:"Léviathan",g:"Frontier",e:"💧 Eau",s:2,h:"Tundra",d:3}, // ~16.1 m
+  {n:"Rathalos Zénith",c:"Wyverne volante",g:"Frontier",e:"🔥 Feu",s:3,h:"Forêt",d:5}, // ~19.5 m
+  {n:"Raviente",c:"Inclassable / ???",g:"Frontier",e:"Aucun",s:4,h:"Océan",d:5}, // ~450.0 m (Colossal)
+  {n:"Raviente agressif",c:"Inclassable / ???",g:"Frontier",e:"Aucun",s:4,h:"Océan",d:5}, // ~450.0 m
+  {n:"Raviente berserk",c:"Inclassable / ???",g:"Frontier",e:"Aucun",s:4,h:"Océan",d:5}, // ~450.0 m
+  {n:"Ray Gougarf",c:"Bête à crocs",g:"Frontier",e:"Aucun",s:2,h:"Forêt",d:4}, // ~13.5 m
+  {n:"Rebidiora",c:"Dragon ancien",g:"Frontier",e:"⚡ Foudre",s:3,h:"Montagne",d:5}, // ~22.6 m
+  {n:"Rukodiora",c:"Dragon ancien",g:"Frontier",e:"Aucun",s:3,h:"Désert",d:5}, // ~22.6 m
+  {n:"Rukodiora Zénith",c:"Dragon ancien",g:"Frontier",e:"Aucun",s:3,h:"Désert",d:5}, // ~24.5 m
+  {n:"Shantien",c:"Dragon ancien",g:"Frontier",e:"💧 Eau / ⚡ Foudre",s:3,h:"Montagne",d:5}, // ~36.2 m
+  {n:"Taikun Zamuza",c:"Carapaceon",g:"Frontier",e:"Aucun",s:2,h:"Cave",d:4}, // ~14.2 m
+  {n:"Taikun Zamuza Zénith",c:"Carapaceon",g:"Frontier",e:"⚡ Foudre",s:3,h:"Cave",d:5}, // ~16.0 m
+  {n:"Tigrex Zénith",c:"Wyverne volante",g:"Frontier",e:"Aucun",s:3,h:"Montagne",d:5}, // ~21.5 m
+  {n:"Toa Tesukatora",c:"Dragon ancien",g:"Frontier",e:"🧊 Glace",s:3,h:"Tundra",d:5}, // ~24.3 m
+  {n:"Toridcless",c:"Wyverne volante",g:"Frontier",e:"⚡ Foudre",s:3,h:"Forêt",d:4}, // ~19.5 m
+  {n:"Toridcless Zénith",c:"Wyverne volante",g:"Frontier",e:"⚡ Foudre",s:3,h:"Forêt",d:5}, // ~21.0 m
+  {n:"Varusaburosu",c:"Wyverne volante",g:"Frontier",e:"🔥 Feu",s:3,h:"Volcan",d:5}, // ~20.7 m
+  {n:"Voljang",c:"Bête à crocs",g:"Frontier",e:"🔥 Feu",s:2,h:"Volcan",d:4}, // ~12.2 m
+  {n:"Yama Kurai",c:"Dragon ancien",g:"Frontier",e:"Aucun",s:4,h:"Forêt",d:5}, // ~41.3 m
+  {n:"Zenaserisu",c:"Wyverne volante",g:"Frontier",e:"Aucun",s:3,h:"Marais",d:4}, // ~23.4 m
+  {n:"Zerureusu",c:"Wyverne volante",g:"Frontier",e:"⚡ Foudre",s:3,h:"Ruines",d:5}, // ~17.0 m
+  {n:"Zerureusu étincelant",c:"Wyverne volante",g:"Frontier",e:"⚡ Foudre",s:3,h:"Ruines",d:5}, // ~17.0 m
+  {n:"Zinogre hurleur",c:"Wyverne à crocs",g:"Frontier",e:"⚡ Foudre / 🐉 Dragon",s:2,h:"Forêt",d:5}, // ~17.5 m
+
+// --- GÉNÉRATION ONLINE ---
+  {n:"Baelidae",c:"Temnocéran",g:"Online",e:"Aucun",s:2,h:"Marais",d:3}, // ~13.5 m
+  {n:"Baelidae d'effroi",c:"Temnocéran",g:"Online",e:"Aucun",s:2,h:"Marais",d:4}, // ~13.5 m
+  {n:"Basarios cristal",c:"Wyverne volante",g:"Online",e:"🔥 Feu",s:2,h:"Volcan",d:3}, // ~14.3 m
+  {n:"Basarios des sables",c:"Wyverne volante",g:"Online",e:"Aucun",s:2,h:"Désert",d:3}, // ~14.3 m
+  {n:"Blangonga de flamme",c:"Bête à crocs",g:"Online",e:"🔥 Feu",s:2,h:"Volcan",d:4}, // ~12.1 m
+  {n:"Bulldrome élémentaire",c:"Bête à crocs",g:"Online",e:"Aucun",s:1,h:"Forêt",d:2}, // ~6.8 m
+  {n:"Caeserber",c:"Bête à crocs",g:"Online",e:"Aucun",s:1,h:"Forêt",d:1}, // ~5.8 m
+  {n:"Caeserber des sables",c:"Bête à crocs",g:"Online",e:"Aucun",s:1,h:"Désert",d:2}, // ~5.8 m
+  {n:"Caeserber fantôme",c:"Bête à crocs",g:"Online",e:"Aucun",s:1,h:"Marais",d:3}, // ~5.8 m
+  {n:"Ceanataur Shogun Maître-lame",c:"Carapaceon",g:"Online",e:"💧 Eau",s:2,h:"Marais",d:4}, // ~12.0 m
+  {n:"Chramine",c:"Wyverne rapace",g:"Online",e:"Aucun",s:2,h:"Forêt",d:3}, // ~13.4 m
+  {n:"Chramine glaciale",c:"Wyverne rapace",g:"Online",e:"🧊 Glace",s:2,h:"Tundra",d:4}, // ~13.4 m
+  {n:"Congalala doré",c:"Bête à crocs",g:"Online",e:"Aucun",s:2,h:"Forêt",d:3}, // ~11.4 m
+  {n:"Estrellian",c:"???",g:"Online",e:"🔥 Feu",s:2,h:"Forêt",d:4}, // ~16.5 m
+  {n:"Estrellian suprême",c:"???",g:"Online",e:"🔥 Feu / 🐉 Dragon",s:2,h:"Ruines",d:5}, // ~17.5 m
+  {n:"Estrellian ténébreux",c:"???",g:"Online",e:"🐉 Dragon",s:2,h:"Ruines",d:4}, // ~16.5 m
+  {n:"Gonngenn Hermitaur",c:"Carapaceon",g:"Online",e:"💧 Eau",s:2,h:"Désert",d:4}, // ~12.5 m
+  {n:"Guren Shen Gaoren",c:"Carapaceon",g:"Online",e:"🔥 Feu",s:4,h:"Volcan",d:5}, // ~34.0 m
+  {n:"Hypnocatrice dorée",c:"Wyverne rapace",g:"Online",e:"Aucun",s:2,h:"Forêt",d:3}, // ~12.9 m
+  {n:"Lightenna",c:"Néoptéron",g:"Online",e:"⚡ Foudre",s:2,h:"Forêt",d:4}, // ~14.2 m
+  {n:"Lightenna stygienne",c:"Néoptéron",g:"Online",e:"🐉 Dragon",s:2,h:"Marais",d:4}, // ~14.2 m
+  {n:"Merphistophelin",c:"Dragon ancien",g:"Online",e:"Aucun",s:3,h:"Ruines",d:5}, // ~28.6 m
+  {n:"Merphistophelin élémentaire",c:"Dragon ancien",g:"Online",e:"🔥 Feu / 🧊 Glace / ⚡ Foudre",s:3,h:"Ruines",d:5}, // ~28.6 m
+  {n:"Monoblos déchu",c:"Wyverne volante",g:"Online",e:"Aucun",s:3,h:"Désert",d:4}, // ~21.2 m
+  {n:"Onimusha",c:"Carapaceon",g:"Online",e:"Aucun",s:2,h:"Marais",d:4}, // ~12.0 m
+  {n:"Rathian infernale",c:"Wyverne volante",g:"Online",e:"🔥 Feu",s:3,h:"Volcan",d:4}, // ~16.3 m
+  {n:"Slicemargl",c:"Wyverne à crocs",g:"Online",e:"Aucun",s:2,h:"Forêt",d:3}, // ~14.5 m
+  {n:"Slicemargl pourpre",c:"Wyverne à crocs",g:"Online",e:"Aucun",s:2,h:"Marais",d:4}, // ~14.5 m
+  {n:"Tartaronis",c:"Dragon ancien",g:"Online",e:"Aucun",s:4,h:"Désert",d:5}, // ~48.0 m
+  {n:"Tartaronis infernal",c:"Dragon ancien",g:"Online",e:"🔥 Feu",s:4,h:"Désert",d:5}, // ~48.0 m
+  {n:"Teppeki Shen Gaoren",c:"Carapaceon",g:"Online",e:"Aucun",s:4,h:"Montagne",d:5}, // ~32.0 m
+  {n:"Wyverne désastreuse",c:"Wyverne volante",g:"Online",e:"Aucun",s:3,h:"Ruines",d:5}, // ~22.0 m
+  {n:"Yian Garuga une-oreille",c:"Wyverne rapace",g:"Online",e:"🔥 Feu",s:2,h:"Forêt",d:4}, // ~12.7 m
+
+// --- GÉNÉRATION STORIES ---
+  {n:"Aenshin",c:"Dragon ancien",g:"Stories",e:"Aucun",s:3,h:"Ruines",d:5}, // ~24.0 m
+  {n:"Epona",c:"Bête à crocs",g:"Stories",e:"Aucun",s:1,h:"Forêt",d:2}, // ~4.5 m
+  {n:"Kumashira",c:"Bête à crocs",g:"Stories",e:"Aucun",s:1,h:"Forêt",d:2}, // ~6.0 m
+  {n:"Kurenai Goukami",c:"Wyverne à crocs",g:"Stories",e:"🔥 Feu",s:2,h:"Volcan",d:4}, // ~15.5 m
+  {n:"Makili Pietru",c:"Dragon ancien",g:"Stories",e:"🐉 Dragon",s:3,h:"Ruines",d:5}, // ~34.0 m
+  {n:"Oltura",c:"Dragon ancien / ???",g:"Stories",e:"Aucun",s:4,h:"Ruines",d:5}, // ~50.0 m (Ailes colossales)
+  {n:"Versa Pietru",c:"Dragon ancien",g:"Stories",e:"Aucun",s:3,h:"Ruines",d:5}, // ~32.0 m
+
+// --- GÉNÉRATION EXPLORE ---
+  {n:"Agnaktor du Purgatoire",c:"Léviathan",g:"Explore",e:"🔥 Feu",s:3,h:"Volcan",d:4}, // ~28.6 m
+  {n:"Barioth gelé",c:"Wyverne volante",g:"Explore",e:"🧊 Glace",s:3,h:"Tundra",d:4}, // ~21.2 m
+  {n:"B.O.W. Uragaan",c:"Wyverne brute",g:"Explore",e:"Aucun",s:3,h:"Volcan",d:4}, // ~20.9 m
+  {n:"Brachydios Evangelion",c:"Wyverne brute",g:"Explore",e:"Blast",s:2,h:"Volcan",d:5}, // ~15.7 m
+  {n:"Brachydios projection glaciale",c:"Wyverne brute",g:"Explore",e:"🧊 Glace",s:2,h:"Tundra",d:4}, // ~15.7 m
+  {n:"Deviljho colossal",c:"Wyverne brute",g:"Explore",e:"🐉 Dragon",s:3,h:"Forêt",d:5}, // ~28.0 m (Gant)
+  {n:"Diablos armure d'acier",c:"Wyverne volante",g:"Explore",e:"Aucun",s:3,h:"Désert",d:4}, // ~20.7 m
+  {n:"Diablos doré",c:"Wyverne volante",g:"Explore",e:"Aucun",s:3,h:"Désert",d:4}, // ~20.7 m
+  {n:"Duramboros explosif",c:"Wyverne brute",g:"Explore",e:"Blast",s:3,h:"Volcan",d:4}, // ~22.1 m
+  {n:"Eo Garudia",c:"Dragon ancien",g:"Explore",e:"🔥 Feu",s:3,h:"Ruines",d:5}, // ~26.0 m
+  {n:"Gigginox virulent",c:"Wyverne volante",g:"Explore",e:"Poison",s:2,h:"Cave",d:3}, // ~13.7 m
+  {n:"Gigginox yoga",c:"Wyverne volante",g:"Explore",e:"Aucun",s:2,h:"Cave",d:3}, // ~13.7 m
+  {n:"Gobul sorcière",c:"Léviathan",g:"Explore",e:"Aucun",s:2,h:"Marais",d:3}, // ~16.4 m
+  {n:"Gore Magala Evangelion",c:"???",g:"Explore",e:"🐉 Dragon",s:3,h:"Ruines",d:5}, // ~18.5 m
+  {n:"Grand Apiposu",c:"Wyverne rapace",g:"Explore",e:"Aucun",s:1,h:"Désert",d:2}, // ~9.0 m
+  {n:"Kirin empereur de la Foudre",c:"Dragon ancien",g:"Explore",e:"⚡ Foudre",s:1,h:"Montagne",d:5}, // ~5.4 m
+  {n:"Lagiacrus conquête terrestre",c:"Léviathan",g:"Explore",e:"Aucun",s:3,h:"Désert",d:4}, // ~26.4 m
+  {n:"Lagombi sorcière",c:"Bête à crocs",g:"Explore",e:"🧊 Glace",s:1,h:"Tundra",d:3}, // ~5.6 m
+  {n:"Mizutsune bulle de tonnerre",c:"Léviathan",g:"Explore",e:"⚡ Foudre",s:3,h:"Marais",d:4}, // ~19.8 m
+  {n:"Mordomunth",c:"Dragon ancien",g:"Explore",e:"🧊 Glace",s:3,h:"Tundra",d:5}, // ~27.5 m
+  {n:"Nargacuga des marées",c:"Wyverne volante",g:"Explore",e:"💧 Eau",s:3,h:"Océan",d:4}, // ~19.5 m
+  {n:"Nefu Garumudo",c:"Dragon ancien",g:"Explore",e:"Aucun",s:3,h:"Désert",d:5}, // ~30.0 m
+  {n:"Plesioth dorade",c:"Wyverne aquatique",g:"Explore",e:"💧 Eau",s:3,h:"Océan",d:3}, // ~25.2 m
+  {n:"Plesioth torrent",c:"Wyverne aquatique",g:"Explore",e:"💧 Eau",s:3,h:"Océan",d:4}, // ~25.2 m
+  {n:"Rajang Enma",c:"Bête à crocs",g:"Explore",e:"🔥 Feu / ⚡ Foudre",s:2,h:"Volcan",d:5}, // ~11.8 m
+  {n:"Rathalos ardent",c:"Wyverne volante",g:"Explore",e:"🔥 Feu",s:3,h:"Volcan",d:4}, // ~17.0 m
+  {n:"Rathalos de la destruction",c:"Wyverne volante",g:"Explore",e:"🔥 Feu",s:3,h:"Ruines",d:5}, // ~17.0 m
+  {n:"Rathian sécheresse",c:"Wyverne volante",g:"Explore",e:"🔥 Feu",s:3,h:"Désert",d:4}, // ~16.3 m
+  {n:"Seregios démoniaque",c:"Wyverne volante",g:"Explore",e:"Aucun",s:2,h:"Désert",d:5}, // ~15.9 m
+  {n:"Tigrex Sanrio",c:"Wyverne volante",g:"Explore",e:"Aucun",s:3,h:"Forêt",d:3}, // ~18.9 m
+  {n:"Tigrex Unité-02",c:"Wyverne volante",g:"Explore",e:"Aucun",s:3,h:"Montagne",d:5}, // ~18.9 m
+  {n:"Uragaan citrouille",c:"Wyverne brute",g:"Explore",e:"🔥 Feu",s:3,h:"Forêt",d:3}, // ~20.9 m
+  {n:"Uragaan gemme",c:"Wyverne brute",g:"Explore",e:"Aucun",s:3,h:"Volcan",d:4}, // ~20.9 m
+  {n:"Uragaan pastèque",c:"Wyverne brute",g:"Explore",e:"Aucun",s:3,h:"Forêt",d:3}, // ~20.9 m
+  {n:"Uragaan putride",c:"Wyverne brute",g:"Explore",e:"Poison",s:3,h:"Marais",d:4}, // ~20.9 m
+  {n:"Volvidon de Noël",c:"Bête à crocs",g:"Explore",e:"🧊 Glace",s:1,h:"Tundra",d:2}, // ~6.6 m
+  {n:"Volvidon étoile noire",c:"Bête à crocs",g:"Explore",e:"Aucun",s:1,h:"Volcan",d:3}, // ~6.6 m
+  {n:"Zinogre FFBE",c:"Wyverne à crocs",g:"Explore",e:"⚡ Foudre",s:2,h:"Ruines",d:4}, // ~15.3 m
+  {n:"Zinogre immortel",c:"Wyverne à crocs",g:"Explore",e:"⚡ Foudre",s:2,h:"Forêt",d:5}, // ~16.5 m
+
+// --- GÉNÉRATION ORAGE ---
+  {n:"Dermaïos",c:"Wyverne volante",g:"Orage",e:"Aucun",s:3,h:"Marais",d:4}, // ~20.0 m
+  {n:"Gilkuza",c:"Dragon ancien / ???",g:"Orage",e:"Aucun",s:3,h:"Ruines",d:5}, // ~28.0 m
+  {n:"Myo Galuna",c:"Dragon ancien",g:"Orage",e:"🔥 Feu / ⚡ Foudre",s:4,h:"Montagne",d:5}, // ~60.0 m
+  {n:"Naco Agul",c:"Wyverne volante",g:"Orage",e:"Aucun",s:2,h:"Forêt",d:3}, // ~14.5 m
